f5027d096885620ef1e2dc4b8b32cd41847c9338
Zayd Hussain <76216005+Zayd3030@users.noreply.github.com>
2025-04-03 00:26:59 +0100

    add routes and default course route

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mustacheExpress = require("mustache-express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./public")));
+
+// View engine setup
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
+
+// Load Routes
+const authRoutes = require("./routes/authRoutes");
+const courseRoutes = require("./routes/courseRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+
+app.use("/", authRoutes);
+app.use("/courses", courseRoutes);
+app.use("/admin", adminRoutes);
+
+// Default route
+app.get("/", (req, res) => {
+    res.redirect("/courses");
+  });
+  
+
+// Start server
 app.listen(3000,() =>{
 console.log("Server listening on port: 3000");
 });
